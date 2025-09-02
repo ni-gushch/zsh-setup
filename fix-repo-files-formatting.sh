ad50baacff4de560b2c17fdfaa6c837b35b282c3
Gushcharin Nikolay Aleksandrovich <ngushcharin@ozon.ru>
2025-09-02 13:32:05 +0300

    fix settings

--- fix-repo-files-formatting.sh
+++ fix-repo-files-formatting.sh
@@ -2,3 +2,5 @@
 
 npx prettier --write .github/dependabot.yml
 npx prettier --write .github/linters/.markdown-lint.yml
+npx prettier --write .github/workflows/lint.yml
+npx prettier --write .github/configs/zizmor.yaml
